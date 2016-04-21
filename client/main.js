fca1b8cc1c651bdcdc00866dd4d0adc51190f998
Dontbetriangle <richard.legg.89@gmail.com>
2016-04-21 22:19:16 +1200

    added basic image upload

--- client/main.js
+++ client/main.js
@@ -15,6 +15,9 @@ Template.hello.onCreated(function helloOnCreated() {
 Template.hello.helpers({
   counter() {
     return moment(Date()).format('dddd');
+  },
+  slider(){
+	return Slides.find();
   }
 });
 
@@ -22,5 +25,28 @@ Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     Reveal.initialize();
+  }
+});
+
+Template.slide.helpers({
+	getOne(){
+		return Slides.findOne({}).url;
+	}
+});
+
+Template.myForm.events({
+  'change .myFileInput': function(event, template) {
+    var files = event.target.files;
+    for (var i = 0, ln = files.length; i < ln; i++) {
+      Images.insert(files[i], function (err, fileObj) {
+        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+      });
+    }
+  }
+});
+
+Template.imageView.helpers({
+  images: function () {
+    return Images.find(); // Where Images is an FS.Collection instance
   }
 });
